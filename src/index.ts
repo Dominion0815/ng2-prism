bf21644ea50129ee5a8c477f34ba9ce1da1a7c59
Dmitriy Shekhovtsov <valorkin@gmail.com>
2016-12-14 17:51:59 +0200

    2.3.1-2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export { SourceDebounceTime } from './ng2-src-directive/src.directive';
 export * from './languages';
-export { CodeblockComponent as Codeblock } from './codeblock.component';
+export * from './ng2-src-directive/src.directive';
+export * from './ng2-src-directive/sourcable';
 export { CodeblockModule } from './codeblock.module';
+export { CodeRendererComponent } from './code-renderer.component';
+export { CodeblockComponent } from './codeblock.component';
